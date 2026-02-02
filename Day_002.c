3b63d089375a9c5711828ffcd598566d2b4e705b
Arnav <arnavniteenyeole@gmail.com>
2026-02-02 09:39:15 +0530

    Day 2

---
+++ Day_002.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,p;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&p);
+    p=p-1;
+    for(int i=p;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    for(int i=0;i<n-1;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
