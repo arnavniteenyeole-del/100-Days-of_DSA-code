5c34c30b37ac2af43211dfdf075a76f61545723e
Arnav <arnavniteenyeole@gmail.com>
2026-02-11 12:16:33 +0530

    Day 11

---
+++ Day_011.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j;
+    int A[10][10], B[10][10], Sum[10][10];
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    /* Addition of matrices */
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            Sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    printf("Sum of the two matrices:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            printf("%d ", Sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
