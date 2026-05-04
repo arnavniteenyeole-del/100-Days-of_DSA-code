80eadcbd59c11241aa8d810e9ee82fbd035b4ba5
Arnav <arnavniteenyeole@gmail.com>
2026-05-04 19:24:49 +0530

    Day 77

---
+++ Day_077.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int v;
+    struct Node* next;
+};
+
+struct Node* adj[1000];
+int visited[1000];
+
+void addEdge(int u, int v) {
+    struct Node* n1 = (struct Node*)malloc(sizeof(struct Node));
+    n1->v = v;
+    n1->next = adj[u];
+    adj[u] = n1;
+
+    struct Node* n2 = (struct Node*)malloc(sizeof(struct Node));
+    n2->v = u;
+    n2->next = adj[v];
+    adj[v] = n2;
+}
+
+void dfs(int u) {
+    visited[u] = 1;
+    struct Node* temp = adj[u];
+    while (temp) {
+        if (!visited[temp->v])
+            dfs(temp->v);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 1; i <= n; i++) adj[i] = NULL;
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    dfs(1);
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED");
+    return 0;
+}
