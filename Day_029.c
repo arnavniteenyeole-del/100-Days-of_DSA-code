8f4df924cf66ab78dc6175f58c6a0efcf3f18b80
Arnav <arnavniteenyeole@gmail.com>
2026-05-04 15:03:33 +0530

    Day 28

---
+++ Day_029.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to rotate the linked list
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int n = 1;
+
+    // Find length and last node
+    while (temp->next != NULL) {
+        temp = temp->next;
+        n++;
+    }
+
+    // Make it circular
+    temp->next = head;
+
+    // Normalize k
+    k = k % n;
+    int steps = n - k;
+
+    // Traverse to (n-k)th node
+    struct Node* newTail = head;
+    for (int i = 1; i < steps; i++) {
+        newTail = newTail->next;
+    }
+
+    // New head
+    struct Node* newHead = newTail->next;
+
+    // Break the circle
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+// Function to print list
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+// Main function
+int main() {
+    int n, k, x;
+
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        struct Node* newNode = createNode(x);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    printList(head);
+
+    return 0;
+}
